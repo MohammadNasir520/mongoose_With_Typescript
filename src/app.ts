4f99b6ead8fe839dcdcc18e099399a67f959d263
MohammadNasir520 <nasirahsan520@gmail.com>
2023-06-12 12:42:49 +0600

    interface and schema creation

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,100 @@
 import express from "express";
+import { Schema, model, connect } from "mongoose";
 import cors from "cors";
 import { Application, Request, Response } from "express-serve-static-core";
 import { NextFunction } from "connect";
 
 const app: Application = express();
 
-// using cors
+// middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  // inserting a test data into mongodb
+
+  /*
+step:1 interface
+step2: Schema
+step3: Model
+step4: Database query
+
+*/
+
+  // creating interface
+
+  interface IUser {
+    id: string;
+    role: "student";
+    password: string;
+    name: {
+      firstName: string;
+      middleName: string;
+      lastName: string;
+    };
+    dataOfBirth?: string;
+    gender: "male" | "female";
+    email?: string;
+    contactNo: string;
+    emergencyContact: string;
+    presentAddress: string;
+    permanentAddress: string;
+  }
+
+  // creating schema using interface
+  const userSchema = new Schema<IUser>({
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    dataOfBirth: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+    email: {
+      type: String,
+    },
+    contactNo: {
+      type: String,
+    },
+    emergencyContact: {
+      type: String,
+    },
+    presentAddress: {
+      type: String,
+    },
+    permanentAddress: {
+      type: String,
+    },
+  });
+
   res.send("mongoose milestone server is running ");
 });
 
